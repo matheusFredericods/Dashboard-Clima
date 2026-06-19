2d487e378e263d846b2bd0882205649346cbe6a1
Matheus <matheusfred95@gmail.com>
2026-06-19 11:50:22 -0300

    configurações inicias do header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
    <div>
-    <h1>Dashboard de Clima</h1>
+    <h1>Página Inicial</h1>
    </div>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+"use client"
+export function Header(){
+    return(
+        <header>
+            <div>
+                Header
+            </div>
+        </header>
+    )
+}
